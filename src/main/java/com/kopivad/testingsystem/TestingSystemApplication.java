1b185f9f6764227bd7be9210b5c8284e16577875
Vadym <kopivad@gmail.com>
2019-11-28 00:55:30 +0200

    Added liquibase support, password bcrypt encoding, created couple of migrations

--- src/main/java/com/kopivad/testingsystem/TestingSystemApplication.java
+++ src/main/java/com/kopivad/testingsystem/TestingSystemApplication.java
@@ -1,7 +1,9 @@
 package com.kopivad.testingsystem;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestingSystemApplication {
--- src/main/java/com/kopivad/testingsystem/controller/SignUpController.java
+++ src/main/java/com/kopivad/testingsystem/controller/SignUpController.java
@@ -5,6 +5,7 @@ import com.kopivad.testingsystem.model.Role;
 import com.kopivad.testingsystem.model.User;
 import com.kopivad.testingsystem.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +16,14 @@ import java.util.Collections;
 @AllArgsConstructor
 public class SignUpController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping(path = "/signup")
     public String signUp(SignUpForm signUpForm) {
         User newUser = new User();
         newUser.setEmail(signUpForm.getEmail());
         newUser.setNickname(signUpForm.getNickname());
-        newUser.setPassword(signUpForm.getPassword());
+        newUser.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
         newUser.setRoles(Collections.singleton(Role.USER));
         userService.saveUser(newUser);
         return "redirect:/login";
