d4d6812773efc87506e81af20047a9852126c810
Vadym <kopivad@gmail.com>
2020-01-01 21:36:54 +0200

    bean validation

--- src/main/java/com/kopivad/testingsystem/form/SignUpForm.java
+++ src/main/java/com/kopivad/testingsystem/form/SignUpForm.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.*;
 
 @AllArgsConstructor
 @Getter
@@ -14,12 +12,12 @@ import javax.validation.constraints.Min;
 public class SignUpForm {
 
     @Email(message = "Invalid email")
+    @NotBlank(message = "Email can not be empty")
     private String email;
 
-    @Min(value = 3, message = "Nickname must be more than 3 letters")
-    @Max(value = 25, message = "Nickname must be less than 25 letters")
+    @NotBlank(message = "Nickname can not be empty")
     private String nickname;
 
-    @Min(value = 5, message = "Password must be more than 3 letters")
+    @NotBlank(message = "Password can not be empty")
     private String password;
 }
